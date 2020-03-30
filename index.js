a6dbcddc0dc0b779a8f242200c355f5302967400
domingo <ing.domingo.sl@growish.com>
2020-03-30 11:40:16 +0200

    Add publish to central

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const { serveHTTP } = require("stremio-addon-sdk");
+require('dotenv').config();
+
+const { serveHTTP, publishToCentral } = require("stremio-addon-sdk");
 
 const addonInterface = require("./addon");
+
+if(process.env.ENVIRONMENT === 'PRODUCTION')
+    publishToCentral('https://stremio-rpan.xyz:43000/manifest.json');
 
 serveHTTP(addonInterface, { port: 7000 });
